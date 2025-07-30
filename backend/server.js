b67937d3b10b646ff7b04d9d60c07fefe0e5880d
PiyushRaval247 <piyushraval2474@gmail.com>
2025-07-30 11:09:56 +0530

    server updated

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const dotenv = require('dotenv');
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
 
 // Load environment variables
 dotenv.config();
@@ -242,5 +243,4 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-
-}); 
+});
